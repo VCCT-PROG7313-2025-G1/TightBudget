f00704c5ef213e12be974d2d9eb25d5a6895b9e3
ST10046280-Blankenberg <st10046280@vcconnect.edu.za>
2025-04-21 15:26:42 +0200

    Added budget goals screen

---
+++ app/src/main/java/com/example/tightbudget/BudgetGoalsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tightbudget
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BudgetGoalsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_budget_goals)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
