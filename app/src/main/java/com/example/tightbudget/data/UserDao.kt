e0e44e25630ca19e1e662eeb241a582db7020880
ST10046280-Blankenberg <st10046280@vcconnect.edu.za>
2025-04-26 21:50:21 +0200

    feat: implement login functionaliy with RoomDB validation

--- app/src/main/java/com/example/tightbudget/data/UserDao.kt
+++ app/src/main/java/com/example/tightbudget/data/UserDao.kt
@@ -10,7 +10,6 @@ interface UserDao {
     @Insert
     suspend fun insertUser(user: User)
 
-    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
-    suspend fun getUserByUsername(username: String): User?
-
+    @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
+    suspend fun getUserByEmail(email: String): User?
 }
